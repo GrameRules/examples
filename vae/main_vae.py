2199a1f97a5661495384d76ad9dcd4640f63e36e
tata4P <ywangcy@ust.hk>
2019-06-06 16:00:51 +0800

    add logger to keep value. write in one to compare

--- vae/main_vae.py
+++ vae/main_vae.py
@@ -35,94 +35,99 @@ test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
     batch_size=args.batch_size, shuffle=True, **kwargs)
 
-def add_noise(img):
-    noise = torch.randn(img.size()) * 0.4
-    noise = noise.to(device)
-    noisy_img = img + noise
-    return noisy_img
 
-class DAE(nn.Module):
+class VAE(nn.Module):
     def __init__(self):
-        super(DAE, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 256),
-            nn.ReLU(True),
-            nn.Linear(256, 64),
-            nn.ReLU(True))
-        self.decoder = nn.Sequential(
-            nn.Linear(64, 256),
-            nn.ReLU(True),
-            nn.Linear(256, 28 * 28),
-            nn.Sigmoid())
+        super(VAE, self).__init__()
+
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        x = self.encoder(x.view(-1, 784))
-        x = self.decoder(x)
-        return x
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
 
 
-model = DAE().to(device)
+model = VAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    a = torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
 
 
 def train(epoch):
     model.train()
     train_loss = 0
-    train_loss_mle = 0
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
-        # add noise
-        noisy_data = add_noise(data)
-        noisy_data = noisy_data.to(device)
         optimizer.zero_grad()
-        recon_batch = model(noisy_data)
-        loss = F.binary_cross_entropy(recon_batch, data.view(-1, 784), reduction='sum')
-        loss_mle = nn.MSELoss()(recon_batch, data.view(-1, 784), reduction='sum')
+        recon_batch, mu, logvar = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
         loss.backward()
         train_loss += loss.item()
-        train_loss_mle += loss_mle.item()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss BCE: {:.6f}, MLE: {:.6f}'.format(
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
-                loss.item() / len(data),
-                loss_mle.item() / len(data)
-            ))
+                loss.item() / len(data)))
 
-    print('====> Epoch: {} Training set loss bce: {:.4f}, mle: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset), train_loss_mle / len(train_loader.dataset)))
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
 
 
 def test(epoch):
     model.eval()
     test_loss = 0
-    test_loss_mle = 0
     with torch.no_grad():
         for i, (data, _) in enumerate(test_loader):
             data = data.to(device)
-            # add noise
-            noisy_data = add_noise(data)
-            noisy_data = noisy_data.to(device)
-            recon_batch = model(noisy_data)
-            test_loss += F.binary_cross_entropy(recon_batch, data.view(-1, 784), reduction='sum')
-            test_loss_mle += nn.MSELoss()(recon_batch, data.view(-1, 784), reduction='sum')
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
             if i == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
-                                        noisy_data[:n],
                                       recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
                 save_image(comparison.cpu(),
-                         'results/dae_rec_' + str(epoch) + '.png', nrow=n)
+                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
     test_loss /= len(test_loader.dataset)
-    test_loss_mle /= len(test_loader.dataset)
-
-    print('====> Test set loss bce: {:.4f}, mle: {:.4f}'.format(test_loss,test_loss_mle))
+    print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
 for epoch in range(1, args.epochs + 1):
     train(epoch)
     test(epoch)
+    with torch.no_grad():
+        sample = torch.randn(64, 20).to(device)
+        sample = model.decode(sample).cpu()
+        save_image(sample.view(64, 1, 28, 28),
+                   'results/sample_' + str(epoch) + '.png')
